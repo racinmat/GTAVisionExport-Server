ca5359ebc13bc814469ab640a6acc88e855cf2b6
racinmat <racinmat@fel.cvut.cz>
2017-11-11 20:09:57 +0100

    Fully working for local network.

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ def test_queue():
 
 def main():
     ThreadedSocket().start()
-    app.run(debug=False, host='localhost', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
     # test_queue()
     pass
     # "START_SESSION"
