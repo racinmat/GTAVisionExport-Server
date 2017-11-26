8062493dd08237edc2b6e8974d0660ee84009ec7
racinmat <racinmat@fel.cvut.cz>
2017-11-26 21:03:01 +0100

    Modified gallery to utilize server-side pagination.

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import time
 import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import paginate
 
 
 class ThreadedSocket:
@@ -106,7 +107,7 @@ def test_queue():
 def main():
     # use_web_server = False
     use_web_server = True
-    ThreadedSocket().start()
+    # ThreadedSocket().start()
     if use_web_server:
         app.run(debug=False, host='0.0.0.0', port=5000)
     else:
@@ -159,14 +160,22 @@ def gallery_count():
 
 @app.route('/gallery/list', methods=['GET'])
 def gallery_list():
-    # files = list(os.listdir(images_dir))
-    files = list([os.path.join('img', i) for i in os.listdir(images_dir)])
-    return json.dumps(files), 200
+    total = str(len(os.listdir(images_dir)))
+    page_number = request.args.get('pageNumber')
+    page_size = request.args.get('pageSize')
+    if page_number is not None and page_size is not None:
+        print("paginated, number: {}, size: {}".format(page_number, page_size))
+        files = list(os.listdir(images_dir))
+        files = paginate.Page(files, page=int(page_number), items_per_page=int(page_size)).items
+        files = list([os.path.join('img', i) for i in files])
+        return json.dumps({'items': files, 'total': total}), 200
+    else:
+        files = list([os.path.join('img', i) for i in os.listdir(images_dir)])
+        return json.dumps({'items': files, 'total': total}), 200
 
 
 if __name__ == '__main__':
     q = queue.Queue(0)
-    images_dir = 'D:\\GTAV_extraction_output\\rgb-jpeg'
-    # images_dir = 'D:\\projekty\\GTA-V-extractors\\output\\rgb-jpeg'
+    images_dir = 'D:\\projekty\\GTA-V-extractors\\output\\rgb-jpeg'
+    # images_dir = 'D:\\GTAV_extraction_output\\rgb-jpeg'
     main()
-
